18d1310e3d382bb21f12e18ae88d6ba6ce4756c9
Vgrotherz <vgrotherz@gmail.com>
2024-03-10 03:12:33 +0700

    upd index server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
-const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
-// Use the cors middleware to allow requests from all origins
+// Allow all origins for CORS (you can restrict this to specific origins if needed)
 app.use(cors());
 
-// Serve static files from the '../../../build' directory of your React application
-app.use(express.static(path.join(__dirname, './build')));
+// Serve static files from the 'build' directory of your React application
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Route handler for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
