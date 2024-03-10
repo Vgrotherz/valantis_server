b33995c8c474971751761a6de6d20d4941833995
Vgrotherz <vgrotherz@gmail.com>
2024-03-10 12:49:00 +0700

    откат до всё с начала логики client server

--- client/src/components/valantisApp/valantisApp.js
+++ client/src/components/valantisApp/valantisApp.js
@@ -1,36 +1,34 @@
 import md5 from 'md5';
-
-const apiUrl = 'https://valantis-server.vercel.app/';
+const apiUrl = 'http://api.valantis.store:40000/';
 const apiPassword = 'Valantis';
-const retryTime = 1000;
+const retryTime = 1000; // время после которого будет повтор получения данных
 
 export const getDataFromApi = async (action, params, retryCount = 0) => {
-  const timestamp = new Date().toISOString().split('T')[0].replace(/-/g, '');
-  try {
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      body: JSON.stringify({
-        action,
-        params
-      }),
-      headers: {
-        "Content-type": "application/json",
-        "X-Auth": md5(`${apiPassword}_${timestamp}`)
-      }
-    });
+    const timestamp = new Date().toISOString().split('T')[0].replace(/-/g, '');
+    try {
+        const response = await fetch(apiUrl, {
+            method: "POST",
+            body:JSON.stringify({
+                action,
+                params
+            }),
+            headers: {
+                "Content-type" : "application/json",
+                "X-Auth" : md5(`${apiPassword}_${timestamp}`)
+            }
+        });
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+        if(!response.ok) {
+            throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data.result;
+    } catch (error) {
+        console.error("Error making API request:", error);
+
+        await new Promise(resolve => setTimeout(resolve, retryTime));
+        return getDataFromApi(action, params, retryCount + 1);
+        // throw error;
     }
-
-    const data = await response.json();
-    return data.result;
-  } catch (error) {
-    console.error("Error making API request:", error);
-
-    await new Promise(resolve => setTimeout(resolve, retryTime));
-    return getDataFromApi(action, params, retryCount + 1);
-  }
-};
-
-export { apiUrl, apiPassword };
+}
--- server/index.js
+++ server/index.js
@@ -1,55 +1,14 @@
-const express = require('express');
-const cors = require('cors');
 const path = require('path');
-const fetch = require('node-fetch');
-const md5 = require('md5');
+const express = require('express');
 
 const app = express();
 
-// Allow all origins for CORS (you can restrict this to specific origins if needed)
-app.use(cors());
-
-// Serve static files from the 'build' directory of your React application
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Route handler for API requests
-app.post('/api', async (req, res) => {
-  const apiUrl = 'http://api.valantis.store:40000/';
-  const apiPassword = 'Valantis';
-  
-  try {
-    const { action, params } = req.body;
-
-    const timestamp = new Date().toISOString().split('T')[0].replace(/-/g, '');
-    const authHeader = md5(`${apiPassword}_${timestamp}`);
-
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Auth': authHeader
-      },
-      body: JSON.stringify({
-        action,
-        params
-      })
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    res.json({ result: data });
-  } catch (error) {
-    console.error('Error making API request:', error);
-    res.status(500).json({ error: error.message });
-  }
-});
+// Serve static files from the '../../../build' directory of your React application
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Route handler for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
