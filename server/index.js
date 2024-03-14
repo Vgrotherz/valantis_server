d326e050dae94e8c4b2cea116c30b9da3857cdb2
Vgrotherz <vgrotherz@gmail.com>
2024-03-14 13:31:36 +0700

    testig server

--- server/index.js
+++ server/index.js
@@ -1,34 +1,15 @@
 const path = require('path');
 const express = require('express');
-const cors = require('cors'); // Import the cors middleware
 
 const app = express();
 
-// Middleware для обработки CORS
-app.use(cors()); // Enable CORS for all routes
 
-// Serve static files from the appropriate directory based on environment
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-} else {
-  app.use(express.static(path.join(__dirname, '../client/public')));
-}
+// Serve static files from the './build' directory of your React application
+app.use(express.static(path.join(__dirname, './build')));
 
-// Добавляем обработчик для OPTIONS запросов
-app.options('*', cors()); // Enable pre-flight for all routes
-
+// Route handler for the root URL
 app.get('/', (req, res) => {
-  if (process.env.NODE_ENV === 'production') {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  } else {
-    res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
-  }
-});
-
-const apiUrl = 'http://api.valantis.store:40000/';
-
-app.get('/api-url', (req, res) => {
-  res.json({ apiUrl }).status(200);
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
